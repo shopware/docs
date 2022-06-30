25f0da3aa887a063dbdea7d270573ad7e9b36e93
Christian Rades <C.Rades@shopware.com>
2022-06-30 15:41:42 +0200

    DX-104 - fix adr workflow

--- update-adrs.sh
+++ update-adrs.sh
@@ -1,14 +1,17 @@
 #! /usr/bin/env bash
 
- git clone --depth 1 git@github.com:shopware/platform.git
+git clone --depth 1 https://github.com/shopware/platform.git
 
 rm -r ./adr
 rm -r ./.gitbook/assets/adr
 cp -r ./platform/adr ./
- rm -rf ./platform
+rm -rf ./platform
 
 deno run --allow-read --allow-write ./update-summary.ts
 deno run --allow-read --allow-write ./format-adrs.ts
+
+ls -l .
+ls -la adr
 
 mkdir -p ./.gitbook/assets/adr
 cp -r adr/assets/* .gitbook/assets/adr/ 
