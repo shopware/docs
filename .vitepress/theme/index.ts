94801c5afd50d64ceb47a6613ac4e6a4f6588a95
Dominic Klein <dominic_klein@yahoo.com>
2022-11-08 17:15:00 +0100

    Removed empty enhanceapp call

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,18 +2,4 @@
 import { h, App } from "vue";
 import { VPTheme } from "vitepress-shopware-docs";
 
-export default Object.assign({}, VPTheme, {
-  Layout: () => {
-    // @ts-ignore
-    return h(VPTheme.Layout, null, {
-      // banner: () => h(Banner),
-      // "content-top": () => h("h1", "We have important Announcement!"),
-      // 'sidebar-top': () => h(PreferenceSwitch),
-      // 'aside-mid': () => h(SponsorsAside),
-      // 'aside-bottom': () => h(VueJobs)
-    });
-  },
-  enhanceApp({ app }: { app: App }) {
-    // app.provide('some-injection-key-if-needed', VALUE)
-  },
-});
+export default Object.assign({}, VPTheme);
