be7e56af0c282b9b376c416b72e356f49db00ffb
Bojan Rajh <b.rajh@shopware.com>
2023-03-24 15:44:12 +0100

    Single line

--- .github/scripts/update-adrs.sh
+++ .github/scripts/update-adrs.sh
@@ -13,6 +13,7 @@ deno run --allow-read --allow-write ./.github/scripts/format-adrs.ts
 mkdir -p ./.gitbook/assets/adr
 mv ./resources/references/adr/assets/* .gitbook/assets/adr/
 find resources/references/adr -type f -name '*md' -not -name '_*' -exec sed -i 's#(\./assets#(../../../.gitbook/assets/adr#' {} \;
+
 # remove once https://gitlab.shopware.com/shopware/6/product/platform/-/merge_requests/10463 is merged
 find resources/references/adr -type f -name '*md' -not -name '_*' -exec sed -i 's#(\.\./assets#(../../../.gitbook/assets/adr#' {} \;
 
