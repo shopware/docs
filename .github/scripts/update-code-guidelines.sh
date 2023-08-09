938f3d2a40537b03abf823eb4bf13c256e29d8b5
Micha Hobert <michahobert@gmail.com>
2023-08-09 10:04:48 +0200

    Test workflow change

--- .github/scripts/update-code-guidelines.sh
+++ .github/scripts/update-code-guidelines.sh
@@ -3,11 +3,14 @@
 git clone --depth 1 https://github.com/shopware/platform.git
 
 # delete dirs
-rm -r ./resources/guidelines/code/core
+rm -r ./resources/guidelines/code
 
 # copy contents
-cp -r ./platform/coding-guidelines/core ./resources/guidelines/code/core
+cp -r ./platform/coding-guidelines/core ./resources/guidelines/code
+
 rm -rf ./platform
 
 deno run --allow-read --allow-write ./.github/scripts/update-code-guidelines-summary.ts
 deno run --allow-read --allow-write ./.github/scripts/format-code-guidelines.ts
+
+find resources/guidelines/code/ -type d -exec touch '{}'/README.md \;
