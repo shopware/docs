9f4c47198139a81fcb82ac87ec83a4e98c078a3a
Micha Hobert <michahobert@gmail.com>
2023-08-10 13:31:21 +0200

    Final change

--- .github/scripts/update-code-guidelines-summary.ts
+++ .github/scripts/update-code-guidelines-summary.ts
@@ -2,63 +2,65 @@ import { walkSync } from "https://deno.land/std@0.145.0/fs/mod.ts";
 import * as path from "https://deno.land/std@0.145.0/path/mod.ts";
 
 
-interface CodeGuidelinesEntry { path: string }
-interface CodeGuidelinesTopic extends CodeGuidelinesEntry { entries: CodeGuidelinesEntry[] }
+interface CodeEntry { path: string }
+interface CodeTopic extends CodeEntry { entries: CodeEntry[] }
 
 const dateRegex = /[0-9]{4}-[0-9]{2}-[0-9]{2}/;
-const codeGuidelineHeading = /\[Core guidelines\]/;
+const codeHeading = /\[Core guidelines\]/;
+const testHeading = /\[Test\]/;
 const generalTopic = 'general';
+
 
 const firstToUpper = (s: string) => `${s[0].toUpperCase()}${s.slice(1)}`;
 const fixName = (s: string) => firstToUpper(s).replaceAll(/api/gi, 'API').replaceAll(/dal/gi, 'DAL');
 
-const topicName = (entry: CodeGuidelinesEntry) => {
+const topicName = (entry: CodeEntry) => {
 	const name = entry.path.split('/')[3] ?? generalTopic;
 	return name.endsWith('.md') ? generalTopic : name;
 }
 const summaryItem = (depth: number, name: string, path: string) => `${' '.repeat(depth * 2)}* [${fixName(name)}](${path})\n`;
 
-const adrTitle = (adr: CodeGuidelinesEntry) => {
-	return path.basename(adr.path)
+const codeTitle = (code: CodeEntry) => {
+	return path.basename(code.path)
 		.replace(dateRegex, '')
 		.replace('.md', '')
 		.replaceAll('-', ' ')
 		.trim();
 };
 
-let ADRs = new Map<string,CodeGuidelinesTopic>();
-for (const entry of walkSync("./resources/guidelines/code", { includeDirs: false, includeFiles: true })) {
+let Guidelines = new Map<string,CodeTopic>();
+for (const entry of walkSync("./resources/guidelines/code/core", { includeDirs: false, includeFiles: true })) {
 	if (path.basename(entry.path).startsWith('_')) continue;
 	if (path.basename(entry.path) === 'README.md') continue;
 
-	const adr: CodeGuidelinesEntry = { path: entry.path };
-	let topic = ADRs.get(topicName(adr));
+	const code: CodeEntry = { path: entry.path };
+	let topic = Guidelines.get(topicName(code));
 	if (!!topic) {
-		topic.entries.push(adr);
+		topic.entries.push(code);
 		topic.entries.sort((a, b) =>  a.path.localeCompare(b.path));
 		continue
 	}
 	const topicDir = topicName(entry) === generalTopic ? '' : '/' + topicName(entry);
 	topic = {
 		path: `resources/guidelines/code/core${topicDir}`,
-		entries: [adr]
+		entries: [code]
 	}
-	ADRs.set(topicName(adr), topic);
+	Guidelines.set(topicName(code), topic);
 }
 
-let adrSummary = summaryItem(1, 'Core coding guidelines', 'resources/guidelines/code/core/README.md');
+let codeSummary = summaryItem(1, 'Core guidelines', 'resources/guidelines/code/core/README.md');
 
-const names = Array.from(ADRs.keys());
+const names = Array.from(Guidelines.keys());
 names.sort();
 
 names.forEach((name) => {
-	const topic = ADRs.get(name);
+	const topic = Guidelines.get(name);
 	const depthDiff = names.length > 1 ? 1 : 0;
 	if (depthDiff > 0) {
-		adrSummary += summaryItem(2, topicName(topic), path.join(topic.path, 'README.md'));
+		codeSummary += summaryItem(2, topicName(topic), path.join(topic.path, 'README.md'));
 	}
-	for (const adr of topic.entries) {
-		adrSummary += summaryItem(2 + depthDiff, adrTitle(adr), adr.path);
+	for (const code of topic.entries) {
+		codeSummary += summaryItem(2 + depthDiff, codeTitle(code), code.path);
 	}
 });
 
@@ -84,12 +86,20 @@ const summary = {
 const lines = Deno.readTextFileSync(filename).split('\n').values();
 
 for (const line of lines) {
-	if (codeGuidelineHeading.test(line)) break;
+	if (codeHeading.test(line)) break;
 	summary.addLine(line)
 }
 
-summary.addLine(adrSummary);
+summary.addLine(codeSummary);
 
+for (const line of lines) {
+	if (testHeading.test(line)) {
+		summary.addLine(line);
+		break;
+	}
+}
+
+for (const line of lines) summary.addLine(line);
 
 const fileWriter = await Deno.open(filename, {truncate: true, write: true});
 summary.writeTo(fileWriter);
---
+++ resources/guidelines/code/core/README.md
@@ -0,0 +1,5 @@
+# Code
+
+Code guidelines is a comprehensive developer handbook that includes dos and don'ts and a clear description of how to write ADRs, unit tests and more. It enhances code readability, reproducibility, and usability.
+
+Refer to the following different sections to know more about it.
