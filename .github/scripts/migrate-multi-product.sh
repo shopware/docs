5125f98a750a43e6b781f8150e1cc5d9e5d0717e
Bojan Rajh <b.rajh@shopware.com>
2026-04-24 10:17:13 +0200

    fix: projects into services

--- .github/scripts/migrate-multi-product.sh
+++ .github/scripts/migrate-multi-product.sh
@@ -10,15 +10,16 @@ mkdir products/frontends || echo "Products > Frontends already created"
 mv products/digital-sales-rooms products/frontends/digital-sales-rooms || echo "DSR already migrated"
 mv products/sales-agent products/frontends/sales-agent || echo "Sales agent already migrated"
 
-mkdir products/projects || echo "Products > Projects already created"
-mv products/Nexus products/projects/nexus || echo "Nexus already migrated"
+mkdir products/services || echo "Products > Services already created"
+mv products/Nexus products/services/nexus || echo "Nexus already migrated"
+touch products/services/shopware-payments.md || echo "Payments already created"
+touch products/services/shopware-analytics.md || echo "Analytics already created"
+touch products/services/shopware-intelligence.md || echo "Intelligence already created"
 
 mkdir products/environments || echo "Products > Environments already created"
 mv products/paas products/environments/paas || echo "PaaS already migrated"
 mv products/saas.md products/environments/saas.md || echo "SaaS already migrated"
 
-
 touch products/tools/mcp.md
-touch products/projects/shopware-payments.md
 touch products/environments/local.md
 touch products/environments/ci.md
